f3bdc083772338c564ca331e44ea6e21a97a69c5
Esra Nur Arıcan <63196356+EsraNurArican@users.noreply.github.com>
2021-01-10 20:33:37 +0300

    Add files via upload
    08.01.2021 tarihinde verilen ödevler
    -2 ile 10000 arası asal sayıları ekrana basma
    -Girilen şehir array icinde var mı yok mu bulma, eğer şehir varsa indexini de ekrana basar.

---
+++ AsalSayılar_DegerAralıgında/AsalSayılar_DegerAralıgında/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsalSayılar_DegerAralıgında
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("2 ile 10000 arasındaki asal sayılar:");
+            //2 ile 10000 arasında asal sayı kontrolü yapılır
+            for(int i = 2; i < 10000; i++)
+            {
+                //asal sayı kontrolü
+                bool flag = true;
+                for (int j=2; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        flag = false;
+                        break;
+                    }
+
+                }
+                //sayı asalsa ekrana bas
+                if(flag==true)
+                    Console.Write(i+"-");
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ VarMıYokMu/VarMıYokMu/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VarMıYokMu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //bir collection üzerinde herhangi bir eleman var mı yok mu arar.
+            string[] sehirler = { "eskisehir", "istanbul", "izmir", "ankara", "sinop", "diyarbakir" };
+            Console.WriteLine("Bir sehir ismi giriniz");
+            string girilenSehir = Console.ReadLine();
+            //flag kullanılmalı
+            bool VarMı = false;
+            int index = 0,i=0;
+            foreach(string sehir in sehirler)
+            {
+               
+                Console.Write(sehir + "-");
+                if (sehir == girilenSehir)
+                {
+                    VarMı = true;
+                    index = i;
+                }
+                i++;
+
+            }
+            Console.WriteLine();
+            if(VarMı)
+                Console.WriteLine("Girilen sehir arrayde var."+ "Index numarası: "+ index);
+            else
+                Console.WriteLine("Girilen sehir arrayde yok.");
+
+            Console.ReadLine();
+        }
+    }
+}
